7182a366ca4da4414a08941caeb137e465ff8bed
Mobinajalali <131045396+Mobinajalali@users.noreply.github.com>
2026-07-04 12:30:46 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN") or "Test"
 print(TOKEN)
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 OFFSET_FILE = os.getenv("OFFSET_FILE", "offset.txt")
